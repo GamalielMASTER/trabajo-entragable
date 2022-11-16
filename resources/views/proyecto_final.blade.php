158dc7d77d4d557ea9425f7783acfe93351dc89d
GamalielMASTER <dual.leon.solis@gmail.com>
2022-11-15 22:47:49 -0500

    proseso final

---
+++ resources/views/proyecto_final.blade.php
@@ -0,0 +1,128 @@
+<?php
+echo " "
+?>
+ <body background=https://static.vecteezy.com/system/resources/previews/002/008/069/non_2x/3d-starry-night-sky-background-free-photo.jpg>
+    <center><h2><strong>LUGARES TURÍSTICOS EN EL DISTRITO DE HUANUCO</strong></h2></center>
+<center>
+    <button type="button" class="btn btn-primary btn-sm"> <a href="https://travel1tours.com/lugares-turisticos-de-peru/" >CLIC AQUI SI QUIERES MAS LUGARES TURISTICOS DE PERU</a></button>
+<head>
+    <style>
+        h2 {
+            color: red;
+            font-size: 96px;
+            
+        }
+        h1 {
+            color:yellow;
+            font-size: 56px;
+        }
+        body {
+
+            background-color:red;
+        }
+        h5 {
+            color:white;
+            width:1000px;
+            height:40%;
+            font-size: 19px;
+        }
+        a{ 
+            Color:blue;
+        }
+        img{
+            border:3px solid #000;
+            border: radius 15px;
+            display:block;
+            width:1000px;
+            height:100%;
+
+        }
+    </style>
+</head>
+
+
+<BODY>
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="cusco" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="lima" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="lugares turísticos de las diferentes regiones del Perú." >
+    <div class="cusco" >
+    <h1><strong>DISTRITO DE SAN MIGUIEL DE CAURICAURI</strong></h1>
+      <img src="http://1.bp.blogspot.com/-FCS2IF3MLis/Uhps3PQGQYI/AAAAAAAAAaU/MZw4Xo2cZZU/w1200-h630-p-k-no-nu/cauri%2Bactual.jpg"  class="d-block w-100"  alt="lugar turistico de san miguiel de cauri"> 
+      <div class="https://web.facebook.com/profile.php?id=100065028543395&hc_ref=ARTw_Ybdgbv7BlDciMNQeW9Wd1SqOr9YKFxLzRXmwKNiVPdL2kmHBq7QaIKm0-YEAsE&fref=nf">
+        <h5>El distrito fue inaugurado el 2 de mayo de 1940 y creado el 26 de diciembre de 1940..
+             <br>El día de su inauguración contó con la visita del diputado de ese entonces, el Dr. Martel Díaz. 
+             <br>Desde esa fecha, cada año se celebra, el 4 de mayo, la fiesta y la competencia de mayordomos de los<br>
+             tres barrios: Yachas, Chaupis y Julca.
+              <br>CAURI TIENE MAS DE 10 LUGARES TURISTICOS ES SUPER FABULOSA.</h5>
+        <a href="https://web.facebook.com/groups/145245202814368" class="underline">CAURI Y SUS ENCANTOS</a> <br>SIGUIENTE.
+        
+      </div>
+    </div>
+    <div class="lugares turísticos de las diferentes regiones del Perú.">
+    <h1>LAS ZONAS TURISTICAS DE BAÑOS</h1>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Ba%C3%B1os_en_verano_desde_la_cruz_00172.jpg/405px-Ba%C3%B1os_en_verano_desde_la_cruz_00172.jpg" class="d-block w-100"  alt="lima">
+      <div class="https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Ba%C3%B1os_en_verano_desde_la_cruz_00172.jpg/405px-Ba%C3%B1os_en_verano_desde_la_cruz_00172.jpg">
+        <h5>El distrito de Baños es uno de los siete que conforman la provincia de Lauricocha,
+              <br> ubicada en el departamento de Huánuco. 
+              <br>en el centro del Perú.<br>
+              Desde el punto de vista jerárquico de la Iglesia católica forma parte de la Diócesis de Huánuco.
+              <br>Es la localidad de Baños situada a 3.309 msnm, en el margen izquierdo del río Nupe, a 112 km al suroeste de la ciudad de Huánuco.</h5>
+              <a href="https://web.facebook.com/groups/237705871859460" class="underline">DISTRITO DE BAÑOS</a> <br> SIGUIENTE.
+      </div>
+    </div>
+    <div class="lugares turísticos de las diferentes regiones del Perú.">
+    <h1>LAS ZONAS  TURISTICA DE RONDOS</h1>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Ciudad_de_Rondos_03150.JPG/435px-Ciudad_de_Rondos_03150.JPG" class="d-block w-100" alt="DISTRITO DE RONDOS">
+      <div class="carousel-caption d-none d-md-block">
+         <h5>El distrito peruano de Rondos es uno de los siete que conforman la provincia de Lauricocha, está ubicada en el departamento de Huánuco, bajo la administración del Gobierno regional de Huánuco, Perú. 
+         <br> Limita por el norte con el Distrito de La Unión (Provincia Dos de Mayo); por el nor este con los distritos de Obas y Chacabamba (Provincia de Yarowilca) <br>
+         por el sur con el Distrito de Baños; por el este con los distritos de San Francisco de Asís y Jivia 
+        <br> La palabra Rondos es una fonemización del vocablo quechua runtu, que en español significa granizo y también huevo de ave<br>
+         esta al lado este del didtrito de jivia<br>
+         El distrito de Rondos fue creado el 27 de diciembre de 1932, mediante ley N° 7665, promulgado siendo Presidente Luis Miguel Sánchez Cerro.</h5>
+         <a href="https://web.facebook.com/rondos.lauricocha.33" class="underline">DISTRITO DE RONDOS</a> <br> SIGUIENTE.
+      </div>
+    </div>
+  </div>
+  <div class="lugares turísticos de las diferentes regiones del Perú.">
+    <h1>LAS ZONAS  TURISTICA DEL DISTRITO DE JIVIA</h1>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Distrito_de_Jivia_05346.jpg/413px-Distrito_de_Jivia_05346.jpg" class="d-block w-100" alt="zona turistica deL DISTRITO DE JIVIA">
+      <div class="carousel-caption d-none d-md-block">
+        <h5><strong>El distrito peruano de Jivia es uno de los siete distritos que conforman la Provincia de Lauricocha <br>
+          ubicada en el departamento de Huánuco, bajo la administración del Gobierno regional de Huánuco <br>
+          Perú. Limita por el norte con los distritos de Rondos y San Francisco de Asís; por el este con la provincia de Huánuco; por el sur con el distrito de Jesús<br>
+          por el oeste con los distritos de Baños y Rondos.</strong></h5>
+         <a href="https://web.facebook.com/profile.php?id=100079121936646" class="underline">DISTRITO DE JIVIA</a> <br> SIGUIENTE.
+      </div>
+    </div>
+  </div>
+  <div class="lugares turísticos de las diferentes regiones del Perú.">
+    <h1>LAS ZONAS  TURISTICA DE LA PROVINCIA DE HUANUCO</h1>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Jes%C3%BAs_Lauricocha_06885.jpg/375px-Jes%C3%BAs_Lauricocha_06885.jpg" class="d-block w-100" alt="zona turistica de la provincia de lauricocha ">
+      <div class="carousel-caption d-none d-md-block">
+         <h5>Jesús es la capital de la provincia peruana de Lauricocha y del distrito homónimo, ubicado en la región Huánuco. Fundada por los españoles en 1572.<br>
+         Sus coordenadas son: 10º 4' 38,23 de latitud sur y 76º 37' 53,87 de longitud oeste. Estábicada en el margen oeste (valle del Ñucón, mirando de sur a norte) del río Lauricocha, a una altura de 3486 msnm lo cual lo convierte en la capital provincial de mayor altitud dentro de la región Huánuco<br>
+         Por su ubicación a orillas del río Lauricocha (el cual da origen al Marañón en las cercanías de la localidad de Rondos, al juntarse con el río Nupe), es punto de partida para visitar la laguna Lauricocha.</h5>
+         <a href="https://web.facebook.com/MuniLauricocha2019" class="underline">jesus</a> <br> siguiente.
+      </div>
+    </div>
+  </div>
+  <div class="lugares turísticos de las diferentes regiones del Perú.">
+    <h1>LAS ZONAS  TURISTICA DE HUANUCO</h1>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Font_del_Parc_de_San_Sebasti%C3%A1n_amb_l%27esgl%C3%A9sia_de_San_Sebasti%C3%A1n_darrera_a_Hu%C3%A1nuco.jpg/375px-Font_del_Parc_de_San_Sebasti%C3%A1n_amb_l%27esgl%C3%A9sia_de_San_Sebasti%C3%A1n_darrera_a_Hu%C3%A1nuco.jpg" class="d-block w-100" alt="zona turistica de huánuco ">
+      <div class="carousel-caption d-none d-md-block">
+         <h5>Huánuco es una ciudad peruana, capital del distrito, la provincia y el departamento homónimos en el centro norte del país<br>
+         La ciudad tiene una población de 235 529 hab., según proyecciones del INEI para 2020.</h5>
+         <a href="https://web.facebook.com/municipalidadhco" class="underline">HUANUCO</a> <br> GRACIAS.
+      </div>
+    </div>
+  </div>
+ 
+</center>  
+
+</div>
+</BODY>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/proyecto_final', function () {
+    return view('proyecto_final');
+});
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
